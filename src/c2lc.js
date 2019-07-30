220fa50147bd1fb71d2942b24280585ff9672db2
Simon Bates <simon@bitstructures.com>
2019-07-30 13:51:00 -0400

    Add two text editors

--- src/c2lc.js
+++ src/c2lc.js
@@ -7,36 +7,96 @@ class ActionHandler {
 }
 
 class Interpreter {
-    constructor(actions, program) {
+    constructor(actions) {
         this.actions = actions;
-        this.program = program;
     }
 
-    run() {
-        for (const action of this.program) {
+    run(program) {
+        for (const action of program) {
             const handler = this.actions[action];
-            if (!handler) {
-                console.log("UNKNOWN ACTION");
+            if (handler) {
+                handler.handleAction(this);
             } else {
-                handler.handleAction.apply(this);
+                console.log("UNKNOWN ACTION");
             }
         }
     }
 }
 
-class RunButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
+class TextSyntax {
+    read(text) {
+        if (text.trim().length === 0) {
+            return [];
+        }
+        return text.trim().split(/\s+/);
     }
 
-    handleClick() {
-        this.props.interpreter.run();
+    print(program) {
+        return program.join(" ");
+    }
+}
+
+class ProgramTextEditor extends React.Component {
+    // This is a 'uncontrolled component' that maintains its own local version
+    // of the program text. The changes are sent outwards (to the
+    // props.onChange handler) at blur. And getDerivedStateFromProps() is used
+    // with an explicit version number to trigger this component to update
+    // its state to reflect changes from outside.
+    // See: https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            programVer: props.programVer,
+            text: props.syntax.print(props.program)
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.programVer !== state.programVer) {
+            return {
+                programVer: props.programVer,
+                text: props.syntax.print(props.program)
+            };
+        } else {
+            return null;
+        }
+    }
+
+    handleChange(e) {
+        // Update the local program text state
+        this.setState({
+            text: e.target.value
+        });
+    }
+
+    handleBlur() {
+        // Call the props.onChange handler at blur.
+        // We could implement a much more sophisticated strategy here, such as
+        // checking if the program is valid at each edit (textarea.onChange)
+        // and call the onChange handler if the program has changed (and it is
+        // valid).
+        this.props.onChange(this.props.syntax.read(this.state.text));
     }
 
     render() {
         return (
-            <button onClick={this.handleClick}>Run</button>
+            <div>
+                <textarea
+                    value={this.state.text}
+                    onChange={this.handleChange}
+                    onBlur={this.handleBlur} />
+            </div>
+        );
+    }
+}
+
+class RunButton extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.onClick}>Run</button>
         );
     }
 }
@@ -44,19 +104,54 @@ class RunButton extends React.Component {
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            program: ["forward", "left", "right", "forward"],
+            programVer: 1
+        };
+
         this.interpreter = new Interpreter(
             {
                 forward: new ActionHandler(() => { console.log("FORWARD") }),
                 left: new ActionHandler(() => { console.log("LEFT") }),
                 right: new ActionHandler(() => { console.log("RIGHT") })
-            },
-            ["forward", "left", "right", "forward"]
+            }
         );
+
+        this.syntax = new TextSyntax();
+
+        this.handleProgramChange = this.handleProgramChange.bind(this);
+        this.handleClickRun = this.handleClickRun.bind(this);
+    }
+
+    handleProgramChange(program) {
+        this.setState((state) => {
+            return {
+                program: program,
+                programVer: state.programVer + 1
+            }
+        });
+    }
+
+    handleClickRun() {
+        this.interpreter.run(this.state.program);
     }
 
     render() {
         return (
-            <RunButton interpreter={ this.interpreter } />
+            <div>
+                <ProgramTextEditor
+                    program={ this.state.program }
+                    programVer={ this.state.programVer }
+                    syntax={ this.syntax }
+                    onChange={ this.handleProgramChange } />
+                <ProgramTextEditor
+                    program={ this.state.program }
+                    programVer={ this.state.programVer }
+                    syntax={ this.syntax }
+                    onChange={ this.handleProgramChange } />
+                <RunButton onClick={ this.handleClickRun } />
+            </div>
         );
     }
 }
